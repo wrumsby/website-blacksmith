9bf4981f754ace9925fb2a797bcd9c08ae73d531
Walter Rumsby <walter@rumsby.net>
2013-05-06 21:21:59 +1200

    More updates

--- server.js
+++ server.js
@@ -1,9 +1,61 @@
 'use strict';
 
 var http = require('http'),
-	port = process.env.PORT || 1337;
+	url = require('url'),
+	path = require('path'),
+	fs = require('fs'),
+	port = process.env.PORT || 1337,
+	extensions = {
+		'.html': 'text/html',
+		'.css': 'text/css'
+	};
+
+function getFile (localPath, mimeType, res) {
+	fs.readFile(localPath, function (err, contents) {
+		if (!err) {
+			res.writeHead(200, {
+				'Content-Type': mimeType,
+				'Content-Length': contents.length
+			});
+			res.end(contents);
+		} else {
+			res.writeHead(500);
+			res.end();
+		}
+	});
+}
+
+function getIndex (res) {
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	res.end('<html><body>Soon</body></html>');	
+}
 
 http.createServer(function (req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello world\n');
+	var filename = path.basename(req.url) || 'index.html',
+		ext = path.extname(filename),
+		localPath = __dirname + '/public/';
+
+	if (filename === 'index.html') {
+		localPath += 'index.html';
+	} else {
+		localPath += url.parse(req.url).pathname.replace(/\+/g, ' ');
+	}
+
+	if (!ext) {
+		ext = '.html';
+		localPath += '/index.html';
+	}
+
+	path.exists(localPath, function (exists) {
+		if (exists) {
+			getFile(localPath, extensions[ext], res);
+		} else if (localPath === __dirname + '/public/index.html') {
+			getIndex(res);
+		} else {
+			res.writeHead(404);
+			res.end();
+		}
+	});
 }).listen(port);
